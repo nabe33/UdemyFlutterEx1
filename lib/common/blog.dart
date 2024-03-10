1510c5e238eaa4f421860dfd3ca8b5159dbddc59
nabe <nabe@lab.twcu.ac.jp>
2024-03-10 21:37:32 +0900

    finished section 19.

--- lib/common/blog.dart
+++ lib/common/blog.dart
@@ -1,8 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 import '../components.dart';
 
